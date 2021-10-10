0985affc88de1382b2bee53d604ec57314530e53
febianareswara <89761118+febianareswara@users.noreply.github.com>
2021-10-10 22:39:35 +0700

    Delete latihan5.py

--- latihan5.py
+++
@@ -1,7 +0,0 @@
-print ('Grafik Diagram Batang Data Jumlah anak Laki-laki dan Perempuan Mahasiswa PTIK UNS 2021')
-print ('Diketahui jumlah mahasiswa laki laki ada 42, jumlah mahasiswa perempuan ada 29')
-jumlahmhs_lk = 42
-jumlahmhs_pr = 29
-print ('Grafik')
-print ('Jumlah mahasiswa laki-laki :', '/' * jumlahmhs_lk,'('+ str(jumlahmhs_lk), ')')
-print ('Jumlah mahasiswa perempuan :', '/' * jumlahmhs_pr,'('+ str(jumlahmhs_pr), ')')
